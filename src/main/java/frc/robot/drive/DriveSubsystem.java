c3e29ba1f710b0faf4620cf6fa78f4e06482091b
Matt <matthew.morley.ca@gmail.com>
2019-12-15 01:59:30 -0800

    s3nd

--- src/main/java/frc/robot/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/drive/DriveSubsystem.java
@@ -1,7 +1,17 @@
 package frc.robot.drive;
 
-public class DriveSubsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class DriveSubsystem extends SubsystemBase {
 
+    SwerveDriveModule flModule = null; // TODO
+    SwerveDriveModule frModule = null; // TODO
+    SwerveDriveModule blModule = null; // TODO
+    SwerveDriveModule brModule = null; // TODO
+
+    @Override
+    public void periodic() {
+        // modules.forEach(SwerveDriveModule::update); // TODO
+    }
 
 }
